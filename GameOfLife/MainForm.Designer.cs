5c643904c0c3148a10cbf0e3b9a03f5392188b6d
IAFEnvoy <tanksheng545@126.com>
2021-10-13 22:09:43 +0800

    添加项目文件。

---
+++ GameOfLife/MainForm.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace GameOfLife
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.开始暂停ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.重新开始ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.开始暂停ToolStripMenuItem,
+            this.重新开始ToolStripMenuItem,
+            this.退出ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(770, 25);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 开始暂停ToolStripMenuItem
+            // 
+            this.开始暂停ToolStripMenuItem.Name = "开始暂停ToolStripMenuItem";
+            this.开始暂停ToolStripMenuItem.Size = new System.Drawing.Size(73, 21);
+            this.开始暂停ToolStripMenuItem.Text = "开始/暂停";
+            this.开始暂停ToolStripMenuItem.Click += new System.EventHandler(this.开始暂停ToolStripMenuItem_Click);
+            // 
+            // 重新开始ToolStripMenuItem
+            // 
+            this.重新开始ToolStripMenuItem.Name = "重新开始ToolStripMenuItem";
+            this.重新开始ToolStripMenuItem.Size = new System.Drawing.Size(68, 21);
+            this.重新开始ToolStripMenuItem.Text = "重新开始";
+            // 
+            // 退出ToolStripMenuItem
+            // 
+            this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
+            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.退出ToolStripMenuItem.Text = "退出";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(11, 72);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(750, 400);
+            this.panel1.TabIndex = 2;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseLeave += new System.EventHandler(this.panel1_MouseLeave);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 28);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 36);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "出生条件：\r\n\r\n存活条件：";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(770, 483);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "GameOfLife";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.Shown += new System.EventHandler(this.MainForm_Shown);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 开始暂停ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 重新开始ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ GameOfLife/MainForm.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace GameOfLife
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        CheckBox[] bores = new CheckBox[10];
+        CheckBox[] survives = new CheckBox[10];
+        bool[,] data = new bool[200, 100];
+        int w = 100, h = 50;
+        int[] kx = { 0, 0, 1, -1, 1, 1, -1, -1 };
+        int[] ky = { 1, -1, 0, 0, 1, -1, 1, -1 };
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i <= 8; i++)
+            {
+                bores[i] = new CheckBox();
+                bores[i].Text = i.ToString();
+                bores[i].Location = new Point(75 + (30 + 5) * i, 25);
+                bores[i].Size = new Size(30, 16);
+                this.Controls.Add(bores[i]);
+            }
+            bores[3].Checked = true;
+            for (int i = 0; i <= 8; i++)
+            {
+                survives[i] = new CheckBox();
+                survives[i].Text = i.ToString();
+                survives[i].Location = new Point(75 + (30 + 5) * i, 50);
+                survives[i].Size = new Size(30, 16);
+                this.Controls.Add(survives[i]);
+            }
+            survives[2].Checked = true;
+            survives[3].Checked = true;
+        }
+
+        private void 开始暂停ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = !timer1.Enabled;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            bool[,] temp = new bool[200, 100];
+            for (int i = 1; i <= w; i++)
+                for (int j = 1; j <= h; j++)
+                {
+                    int lcnt = 0;
+                    for (int k = 0; k < 8; k++)
+                        lcnt += data[i + kx[k], j + ky[k]] ? 1 : 0;
+                    temp[i, j] = data[i, j] ? survives[lcnt].Checked : bores[lcnt].Checked;
+                }
+            for (int i = 1; i <= w; i++)
+                for (int j = 1; j <= h; j++)
+                    data[i, j] = temp[i, j];
+            Graphics g = panel1.CreateGraphics();
+            g.Clear(Color.White);
+            for (int i = 0; i < 100; i++)
+                for (int j = 0; j < 50; j++)
+                    if (data[i, j])
+                        g.FillRectangle(Brushes.Black, i * 10 + 1, j * 10 + 1, 8, 8);
+        }
+
+        bool down;
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            down = true;
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!down) return;
+            try
+            {
+                if (e.Button == MouseButtons.Left)//左键负责绘制
+                    data[e.Location.X / 10, e.Location.Y / 10] = true;
+                if (e.Button == MouseButtons.Right) //右键负责擦除
+                    data[e.Location.X / 10, e.Location.Y / 10] = false;
+            }
+            catch { 
+                down = false; 
+            }
+            Graphics g = panel1.CreateGraphics();
+            g.Clear(Color.White);
+            for (int i = 0; i < 100; i++)
+                for (int j = 0; j < 50; j++)
+                    if (data[i, j])
+                        g.FillRectangle(Brushes.Black, i * 10 + 1, j * 10 + 1, 8, 8);
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            down = false;
+        }
+
+        private void MainForm_Shown(object sender, EventArgs e)
+        {
+            Graphics g = panel1.CreateGraphics();
+            g.Clear(Color.White);
+        }
+
+        private void panel1_MouseLeave(object sender, EventArgs e)
+        {
+            down = false;
+        }
+    }
+}
